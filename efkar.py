70318558bf29552c76dbfec6447ed8bfb5da6845
mrpleak <87371789+mrpleak@users.noreply.github.com>
2021-07-21 21:03:15 +0300

    Add files via upload

---
+++ efkar.py
@@ -0,0 +1,68 @@
+import webbrowser
+import os
+
+print("-----------------------------------")
+print("      Bozkurt Efkar Tool'u         ")
+print("                                   ")
+print("-----------------------------------")
+print()
+
+print("1.Müslüm baba Unutamadım")
+print("2.Ruhumda Sızı")
+print("3.Aldırma Gönül")
+print("4.Öldümde Uyandım Gülüm")
+print("5.Kendine iyi bak")
+print("6.Adaletin Bumu Dünya")
+print("7.Ben Yoruldum Hayat")
+print("8.Müslüm Baba Affet")
+print("9.Müslüm Baba Ayrılık Acı Bir şey")
+print("10.Müslüm Baba Hangimiz Sevmedik")
+print("11.Ali Kınık Üzülmedin mi?")
+print("12.Bilsen Şimdi Nerdeyim Çılgın Gecelerdeyim...")
+print("13.Bazen Vice")
+print("14.Bazen2 Vice")
+
+
+efkar = input("ne istersin be gardaşım :")
+
+if efkar =="1":
+	os.system("termux-open-url https://www.youtube.com/watch?v=lYNMQBKDhwg")
+
+if efkar =="2":
+	os.system("termux-open-url https://www.youtube.com/watch?v=67UWF7eMSi8")
+
+if efkar =="3":
+	os.system("termux-open-url https://www.youtube.com/watch?v=8yk0dNCsN2Y")
+
+if efkar =="4":
+	os.system("termux-open-url https://www.youtube.com/watch?v=wgNQ65MippE")
+
+if efkar =="5":
+	os.system("termux-open-url https://www.youtube.com/watch?v=Oxx2BsC7jEc&t=58s")
+
+if efkar =="6":
+	os.system("termux-open-url https://www.youtube.com/watch?v=wt2wOLbRKDA")
+
+if efkar =="7":
+	os.system("termux-open-url https://www.youtube.com/watch?v=K7z3590-Mt0")
+
+if efkar =="8":
+	os.system("termux-open-url https://www.youtube.com/watch?v=2YYLr6yVruQ")
+
+if efkar =="9":
+	os.system("termux-open-url https://www.youtube.com/watch?v=cITUab_6sVs")
+
+if efkar =="10":
+	os.system("termux-open-url https://www.youtube.com/watch?v=IRIMVXyAvw0")
+
+if efkar =="11":
+	os.system("termux-open-url https://www.youtube.com/watch?v=HctKOy1DXCc")
+
+if efkar =="12":
+	os.system("termux-open-url https://www.youtube.com/watch?v=IHWfVSoBKic")
+
+if efkar =="13":
+	os.system("termux-open-url https://www.youtube.com/watch?v=51jwCBO_R6I")
+
+if efkar =="14":
+	os.system("termux-open-url https://www.youtube.com/watch?v=1KdEjbDjB-s")
